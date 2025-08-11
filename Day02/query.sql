2b00267e0854e421dd4f5662a730f6fab1b4796f
Vannamathi B <95581861+Vannamathi@users.noreply.github.com>
2025-08-11 14:56:02 +0530

    Create query.sql

---
+++ Day02/query.sql
@@ -0,0 +1,8 @@
+-- DAY 2
+
+# WHERE Clause: Used to filter rows based on a specific condition.
+
+# Fetch details of all employees whose salary is greater than 50,000.
+
+SELECT * FROM EMPLOYEES 
+	WHERE SALARY > 50000;
